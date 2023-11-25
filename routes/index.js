1672324c55e0dcf7b46cf5960c552b7a3783885f
Carlos Garcia <corzuelachaco2019@gmail.com>
2023-11-25 17:23:52 -0300

    pimienta&sal

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+const {index, detail}=require('../controllers/indexController')
+/* GET home page. */
+
+router.get('/',index);
+router.get('/detalle/:id',detail)
+// router
+//     .get('/',index)
+//     .get('./detalle/:id',detail)
+
+module.exports = router;
